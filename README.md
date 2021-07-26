3eba9508dbed4384749fa2e160d6935b2f43e06f
carloschicas <carlos.chicas@503consulting.com>
2021-07-25 22:06:58 -0600

    Changes for 25/07/2021

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 This is the finance tracker app from the Complete Ruby on Rails Developer course
 
 Course repository: https://github.com/udemyrailscourse/finance-tracker-6
+
+To edit the credentials in windows, you need to run
+
+$env:EDITOR="notepad"
+
+In PowerShell, then run "rails credentials:edit" in the same window.
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_21_061254) do
+ActiveRecord::Schema.define(version: 2021_07_25_021623) do
+
+  create_table "stocks", force: :cascade do |t|
+    t.string "ticker"
+    t.string "name"
+    t.decimal "last_price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
